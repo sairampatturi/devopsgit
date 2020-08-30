6f852e5ae3e77c73c1263c5789e2a7d98239e334
Sairam Patturi (spatturi) <spatturi@micron.com>
2020-08-30 21:44:53 +0530

    first time commit changes

---
+++ primepgm.py
@@ -0,0 +1,21 @@
+# taking input from user
+number = int(input("Enter any number: "))
+r1 = int(input("enter the range number"))
+
+# prime number is always greater than 1
+if number > 1:
+
+    for i in range(r1, number + 1):
+
+        for j in range(2, i):
+
+            if (i % j) == 0:
+                # print(j, "is not a prime number")
+                continue
+            else:
+                print(i, "is a prime number")
+
+# if the entered number is less than or equal to 1
+# then it is not prime number
+else:
+    print(number, "is not a prime number end")
